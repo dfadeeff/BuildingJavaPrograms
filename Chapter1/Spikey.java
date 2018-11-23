dd85eaf6b4f6f4dd84b00285e643c357cd0c0da3
Fadeeff <fadejeff@gmail.com>
2018-11-23 23:58:26 +0100

    Add files via upload

---
+++ Chapter1/Spikey.java
@@ -0,0 +1,24 @@
+package Exercises;
+
+public class Spikey{
+	
+	public static void main(String[] args) {
+		top();
+		bottom();
+	}
+	
+	public static void top() {
+		System.out.println("  \\/ ");
+		System.out.println(" \\\\// ");
+		System.out.println("\\\\\\/// ");
+		
+	}
+	
+	public static void bottom() {
+		System.out.println("///\\\\\\ ");
+		System.out.println(" //\\\\ ");
+		System.out.println("  /\\ ");
+		
+	}
+	
+}
